bdddbd8c3db41f9310d76d329527308b26806295
Jagmeet Singh <jagmeettakkar.3599@gmail.com>
2023-07-20 09:34:51 +0530

    blink codes added

---
+++ Codes/Embedded C vs Arduino IDE code/blink.c
@@ -0,0 +1,14 @@
+#include<avr/io.h>
+#include<util/delay.h>
+
+int main()
+{
+    DDRB |= (1<<PB0); 
+    while (1){ 
+        PORTB |= (1<<PB0);
+        _delay_ms(500);
+        PORTB &= ~(1<<PB0);
+        _delay_ms(500); 
+        } 
+    return 0;
+ }
